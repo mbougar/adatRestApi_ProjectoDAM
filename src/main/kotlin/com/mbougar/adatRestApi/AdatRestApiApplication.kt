b7b92b3163ffd45fb9dfcd24e9b37df5d81a00af
mbougar <mbougar303@g.educaand.es>
2024-12-09 22:08:08 +0100

    Initial commit

---
+++ src/main/kotlin/com/mbougar/adatRestApi/AdatRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mbougar.adatRestApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdatRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdatRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mbougar/adatRestApi/AdatRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mbougar.adatRestApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdatRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
